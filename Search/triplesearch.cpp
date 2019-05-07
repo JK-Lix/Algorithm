84bbd1fb56ea2a1820a156298eef2f895b7ebdde
JK-Lix <776579471@qq.com>
2019-05-07 21:26:18 +0800

    add a file

--- Search/triplesearch.cpp
+++ Search/triplesearch.cpp
@@ -1,14 +1,16 @@
 /*************************************************************************
-	> File Name: bubble_sort.cpp
+	> File Name: triplesearch.cpp
 	> Author: Lix
 	> Mail: 776579471@qq.com
 	> URL: https://github.com/JK-Lix
-	> Created Time: 2019年05月07日 星期二 20时35分30秒
+	> Created Time: 2019年05月07日 星期二 21时01分48秒
  ************************************************************************/
 
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
+
+//求解区间极值
 
 int main() {
 
